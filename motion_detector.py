cbe344535577c2d2cfd5f00735c05ac88bd2c059
Luke Weaver <35945561+lukejweaver@users.noreply.github.com>
2021-04-26 23:05:10 -0400

    Update correct motion_detector

--- motion_detector.py
+++ motion_detector.py
@@ -1,16 +1,14 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 def setmode_bcm():
-    1
-    # GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
 
 class MotionSensor:
     def __init__(self, gpio=None):
         self.gpio = gpio
-        # GPIO.setup(self.gpio, GPIO.IN)
+        GPIO.setup(self.gpio, GPIO.IN)
 
     def sensor_status(self):
-        return 0
-        # return GPIO.input(self.gpio)
+        return GPIO.input(self.gpio)
